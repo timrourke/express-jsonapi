75cc78751b3949cab2c4e4e8a4f0fdf4cd79b131
Tim Rourke <tim@timrourke.com>
2017-04-01 08:09:26 -0500

    Adding Post model

---
+++ server/db/migrations/20170401115426-posts-create.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('posts', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      },
+      body: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      }
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable('users');
+  }
+};
---
+++ server/models/Post.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+
+module.exports = {
+  createdAt: {
+    type: Sequelize.DATE
+  },
+  updatedAt: {
+    type: Sequelize.DATE
+  },
+  body: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+};
