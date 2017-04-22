c98696f91cffeab7a16ab377732c670552b9c72c
Tim Rourke <tim@timrourke.com>
2017-04-21 21:05:57 -0500

    Adding Unprocessable Entity object

---
+++ server/jsonapi/errors/UnprocessableEntity.js
@@ -0,0 +1,42 @@
+'use strict';
+
+class UnprocessableEntity extends Error {
+
+  /**
+   * Set the source.pointer property on the request object
+   *
+   * @see http://jsonapi.org/format/#errors
+   *
+   * @param {String} pointer The path to the invalid query parameter
+   */
+  setPointer(pointer) {
+    this.source = {
+      pointer: pointer
+    };
+  }
+
+  /**
+   * toJSON
+   *
+   * @return {Object}
+   */
+  toJSON() {
+    let ret = {
+      status: 422,
+      title: 'Unprocessable Entity',
+      detail: this.message
+    };
+
+    if (this.hasOwnProperty('links')) {
+      ret.links = this.links;
+    }
+
+    if (this.hasOwnProperty('source')) {
+      ret.source = this.source;
+    }
+
+    return ret;
+  }
+}
+
+module.exports = UnprocessableEntity;
