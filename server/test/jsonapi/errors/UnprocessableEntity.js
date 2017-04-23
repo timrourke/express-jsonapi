af51992ae2ee12e3c7bad03b41bbd8f3fa479423
Tim Rourke <tim@timrourke.com>
2017-04-23 09:17:44 -0500

    Adding test for UnprocessableEntity

---
+++ server/test/jsonapi/errors/UnprocessableEntity.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const UnprocessableEntity = require('./../../../jsonapi/errors/UnprocessableEntity');
+
+chai.should();
+
+describe('jsonapi/errors/UnprocessableEntity', () => {
+  describe('#toJSON()', () => {
+    it('should serialize to correct JSON', () => {
+      let actual = new UnprocessableEntity('I just can\'t even');
+
+      let expected = {
+        status: 422,
+        title: 'Unprocessable Entity',
+        detail: 'I just can\'t even',
+      };
+
+      JSON.stringify(actual).should.be.eql(JSON.stringify(expected));
+    });
+
+    it('should serialize to correct JSON with links and source', () => {
+      let actual = new UnprocessableEntity('Hooowhee that thing is BROKE');
+      actual.setPointer('/things/other-things/this-one');
+      actual.links = 'linkz r kewl';
+
+      let expected = {
+        status: 422,
+        title: 'Unprocessable Entity',
+        detail: 'Hooowhee that thing is BROKE',
+        links: 'linkz r kewl',
+        source: {
+          pointer: '/things/other-things/this-one'
+        }
+      };
+
+      JSON.stringify(actual).should.be.eql(JSON.stringify(expected));
+    });
+  });
+});
