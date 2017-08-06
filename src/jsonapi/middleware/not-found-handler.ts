fae7878149c8648c7dd9e32d2165900eeb463d85
Tim Rourke <tim@timrourke.com>
2017-08-06 08:16:30 -0500

    Add docblock

--- src/jsonapi/middleware/not-found-handler.ts
+++ src/jsonapi/middleware/not-found-handler.ts
@@ -4,7 +4,15 @@ import NotFoundError from './../errors/NotFoundError';
 import config from './../../config/config';
 import { NextFunction, Request, Response } from 'express';
 
-export default function(req: Request, res: Response, next: NextFunction) {
+/**
+ * Renders a 404 Not Found error
+ * 
+ * @param {Express.Request} req 
+ * @param {Express.Response} res 
+ * @param {Express.NextFunction} next 
+ * @return {void}
+ */
+export default function(req: Request, res: Response, next: NextFunction): void {
   res.status(404).json({
     errors: [
       new NotFoundError(`Nothing found at ${config.getBaseUrl()}${req.originalUrl}`)
