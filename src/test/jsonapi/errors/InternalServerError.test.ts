5a0e26afc9e381e23d0e94bc0d8c681cf4b1fead
Tim Rourke <tim@timrourke.com>
2017-09-10 07:26:03 -0500

    Fixing tslint errors

--- src/test/jsonapi/errors/InternalServerError.test.ts
+++ src/test/jsonapi/errors/InternalServerError.test.ts
@@ -2,7 +2,7 @@
 
 process.env.NODE_ENV = 'test';
 
-const chai = require('chai');
+import chai = require('chai');
 import InternalServerError from './../../../jsonapi/errors/InternalServerError';
 
 chai.should();
@@ -10,14 +10,15 @@ chai.should();
 describe('jsonapi/errors/InternalServerError', () => {
   describe('#toJSON()', () => {
     it('should serialize to correct JSON', () => {
-      let actual = new InternalServerError();
-      let expected = {
+      const actual = new InternalServerError();
+      const expected = {
+        detail: 'There was an internal error processing your request. Please try again, or contact the system administrator.',
         status: 500,
         title: 'Internal Server Error',
-        detail: 'There was an internal error processing your request. Please try again, or contact the system administrator.'
       };
 
-      JSON.stringify(actual).should.be.eql(JSON.stringify(expected));
+      JSON.parse(JSON.stringify(actual))
+        .should.be.eql(JSON.parse(JSON.stringify(expected)));
     });
   });
 });
